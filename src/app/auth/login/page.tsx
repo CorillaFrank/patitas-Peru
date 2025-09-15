98031a12e8381cf4be3b0d2172607b41488f9257
Frank <jhoncorilla2014@gmail.com>
2025-09-15 12:18:25 -0500

    proyecto configurado

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@radix-ui/themes";
+import { BookmarkIcon } from "@radix-ui/react-icons";
+function loginPage() {
+  //componente basico de React
+  return (
+    <div>
+      LoginPage
+      <Button>
+        <BookmarkIcon /> Login
+      </Button>
+    </div>
+  );
+}
+export default loginPage;
---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,6 @@
+function registerPage() { //componente basico de React
+    return (
+        <div>registerPage</div>    
+    );
+}
+export default registerPage;
