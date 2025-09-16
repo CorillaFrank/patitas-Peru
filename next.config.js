6d47fa3904e522f258df0e0dd3ca9c22dc2fa1fd
Frank <jhoncorilla2014@gmail.com>
2025-09-16 02:31:40 -0500

    login patitasPeru

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // tu configuración aquí
+};
+
+module.exports = nextConfig;
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,14 +1,27 @@
-import { Button } from "@radix-ui/themes";
-import { BookmarkIcon } from "@radix-ui/react-icons";
+import { Container, Card, Heading, Flex, Text, Link } from "@radix-ui/themes";
+import SigninFrom from "@/components/SigninFrom";
+import NavLink from "next/link";
 function loginPage() {
   //componente basico de React
   return (
-    <div>
-      LoginPage
-      <Button>
-        <BookmarkIcon /> Login
-      </Button>
-    </div>
+    <>
+      <Container size="1" height="100%" className="p-3 md:p-0 ">
+        <Flex className="h-screen w-full items-center">
+          <Card className="w-full p7">
+            <Heading>Sing in</Heading>
+            <SigninFrom />
+            <Flex justify="between" my="4">
+              <Text>No tengo una cuenta?</Text>
+              <Link asChild>
+                <NavLink href="/auth/register" passHref>
+                  Registrate!
+                </NavLink>
+              </Link>
+            </Flex>
+          </Card>
+        </Flex>
+      </Container>
+    </>
   );
 }
 export default loginPage;
